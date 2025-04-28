bc976b2ddb5577066b83319bea20dfc55ee0d190
Piyush1617s <piyushsharma9879988@gmail.com>
2025-04-28 23:53:09 +0530

    Add PLANet/main.js 1745864588266

---
+++ PLANet/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'assets', 'icon.png'),
+    title: 'PLANet - Study Planner'
+  });
+
+  win.loadFile('index.html');
+
+  // Open DevTools optionally
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
